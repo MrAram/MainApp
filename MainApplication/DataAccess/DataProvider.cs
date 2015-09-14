ed9adaa6efd00e7f5eb87205a342f7f8c4570217
aram.avetisyan <aram.avetisyan@margasoft.com>
2015-09-14 16:22:24 +0400

    Revert "add access db"
    This reverts commit d782f2e3049ddf22cb05e24c528a6467cbf7c11b.

--- MainApplication/DataAccess/DataProvider.cs
+++ MainApplication/DataAccess/DataProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.OleDb;
 using System.Linq;
 using XMLDatabase.XMLDatabase;
 
@@ -13,37 +12,24 @@ namespace MainApplication.DataAccess
 
         public static bool AddComment(string comment)
         {
-            //string connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + System.Web.HttpContext.Current.Server.MapPath("~/App_Data/Database.accdb");
-            //using (OleDbConnection connection = new OleDbConnection(connString))
-            //{
-            //    connection.Open();
-            //    OleDbCommand command = new OleDbCommand(string.Format("INSERT INTO Comments (ID, Comment) VALUES ({0});", comment), connection);
-            //    command.ExecuteNonQuery();
-            //    //command.Parameters.AddWithValue("@1", userName)
+            var dataset = Data.GetData();
 
-            //}
+            var newRow = dataset.Tables["Comments"].NewRow();
+            newRow["Content"] = comment;
+            dataset.Tables["Comments"].Rows.Add(newRow);
+
+            Data.Save();
 
             return true;
         }
 
         public static List<string> GetComments()
         {
-            var list = new List<string>();
-            string connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + System.Web.HttpContext.Current.Server.MapPath("~/App_Data/Database.accdb");
-            using (OleDbConnection connection = new OleDbConnection(connString))
-            {
-                connection.Open();
-                OleDbDataReader reader = null;
-                OleDbCommand command = new OleDbCommand("SELECT * from  Comments", connection);
-                //command.Parameters.AddWithValue("@1", userName)
-                reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    list.Add(reader[1].ToString());
-                }
-            }
-           
-            return list;
+            var dataset = Data.GetData();
+            var table = dataset.Tables["Comments"];
+
+            var query = table.AsEnumerable().Select(tb => tb.Field<string>("Content"));
+            return query.ToList();
         } 
     }
 }
